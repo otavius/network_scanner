77bfef3797d89f9d3098b4243d6bc4c9893897dd
Otavius <67792940+otavius@users.noreply.github.com>
2023-10-28 22:25:18 -0400

    Update network_scanner.py
    found error within my code needed to update

--- network_scanner.py
+++ network_scanner.py
@@ -11,7 +11,7 @@ def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
-    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[1]
 
     client_lists = []
     for element in answered_list:
